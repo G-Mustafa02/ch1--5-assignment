33aacd5e30750f4b32943c99a441b8172ffb0542
G-Mustafa02 <uzairmustafa963@gmail.com>
2024-09-21 20:15:09 -0700

    Add files via upload

---
+++ chap 3/app.js
@@ -0,0 +1,28 @@
+// var age = "i am 22 y old"
+// alert(age)
+
+// var birthyear = 2002
+// console.log("My birth year is " + birthyear  )
+
+
+
+var visiterName = prompt("enter your name")
+var productTitle = prompt("enter product name")
+var quantity = prompt("enter quantity")
+
+console.log(visiterName+ " order "  + quantity + productTitle)
+// var result = visiterName+ " order "  + quantity + productTitle
+
+var order = document.querySelector("#order")
+order.innerHTML = visiterName+ " order "  + quantity + productTitle
+
+
+
+
+
+
+
+
+
+
+
---
+++ chap 5/app.js
@@ -0,0 +1,146 @@
+// var head = document.querySelector("#head")
+
+// var num1 = prompt("enter your 1st number")
+// var num2 = prompt("enter your 2nd number")
+
+// var total = +num1 + +num2
+
+// head.innerHTML = "sum of " + +num1 + " and " + +num2 + " is " + total ;
+
+
+
+// var number;
+// var head = document.querySelector("#head")
+// head.innerHTML =  "Value after variable declaration is " + number
+// number = 45
+// var para = document.querySelector("#para")
+// para.innerHTML =  "Initial value" + number;
+// number++;
+// var para1 = document.querySelector("#para1")
+// para1.innerHTML = ("value after incrment" + number + "<br/>")
+// number += 7;
+// para1.innerHTML += "value after addition of 7 is" + number + "<br/>"
+// number--;
+// para1.innerHTML += "value after decrement is" + number  + "<br/>"
+// var reminder = number / 3;
+// para1.innerHTML += "thr reminder is" + reminder
+
+
+
+
+
+// var ticketCost = 600;
+// var numberofticket = 10;
+// var totalCost = ticketCost * numberofticket;
+// var cost = document.querySelector("#cost")
+// cost.innerHTML = "total cost of buying 10 ticket is " + totalCost
+
+
+
+
+
+// var temp = 32;
+// var cenToFar = (+temp * 9 / 5) + 32
+// var farTocen = (+temp - 32 ) * 5 / 9
+// console.log(cenToFar)
+// console.log(farTocen);
+
+
+// The Geometrizer
+// var radius = 45;
+// var Circumference = 2 * 3.14 * radius 
+// console.log(Circumference)
+// var area = 3.14 * radius**2
+// var  total = document.querySelector("#total")
+// total.innerHTML = "area of the circle is " + area
+
+
+
+// AGE CALCULATOR
+// var currentYear = 2024
+// var birthYear = 2002
+// var totalAge = +currentYear - +birthYear
+// console.log(totalAge);
+
+// var calculate = document.querySelector("#calculate")
+// calculate.innerHTML = "your current age is " + totalAge
+
+
+
+
+// var dollar = 10;
+// var dollarRate = 278
+// var pkr = dollar * dollarRate
+// console.log(pkr)
+
+
+// var num = document.querySelector("#usernum");
+// console.log(num.value);
+// var text = document.querySelector("#calculate")
+
+
+// function getTable(){
+// for(var i = 1; i <=10; i++){
+//     console.log(+num.value + " * " + i + "=" + +num.value * i)
+//     text.innerHTML += +num.value + " * " + i + "=" + +num.value * i + "</br>"
+// }
+// num.value = ""
+// }
+
+
+// var list = document.querySelector("ol")
+
+// var priceItm1 = 50
+// list.innerHTML += `<li>price of item 1 is ${priceItm1}</li>`
+// var qnty1 = 5
+// list.innerHTML += `<li>quantity of item 1 is ${qnty1}</li>`
+// var priceItm2 = 80
+// list.innerHTML += `<li>price of item 2 is ${priceItm2}</li>`
+// var qnty2 = 10
+// list.innerHTML += `<li>quantity of item 2 is ${qnty2}</li>`
+// list.innerHTML += `total cost is ${+priceItm1 * +qnty1 + +priceItm2 * +qnty2}`
+
+
+// var result = document.querySelector("p")
+
+// var totoalMarks = 500
+// result.innerHTML = `total marks = ${totoalMarks}  </br>`
+// var obtainMarks = 350
+// result.innerHTML += `obtained marks = ${obtainMarks} + </br>`
+// var percentage = obtainMarks / totoalMarks * 100
+// result.innerHTML += `percentage = ${percentage}`
+// console.log(percentage);
+
+
+
+// var num = 15
+// console.log(num * 10 / 2);
+
+// var text = document.querySelector("p")
+// var favSnack = prompt("enter your fav snack")
+// text.innerHTML = `your favourite snak is ${favSnack} </br>`
+// var curAge = prompt("enter your current age")
+// text.innerHTML += `your current age is ${curAge} </br>`
+// var estimateAge = prompt("enter your estimate  age")
+// text.innerHTML += `your estimated age is ${estimateAge} </br>`
+// var numSnackDay = prompt("enter your daily snak intake")
+// text.innerHTML += `your daily snack intake is ${numSnackDay} </br>` 
+// var totalAge = (+estimateAge - +curAge) * 365
+// var numSnakcalculate = +totalAge * +numSnackDay
+// console.log(numSnakcalculate);
+// text.innerHTML += `you would eat ${+numSnakcalculate} snack till your ${estimateAge} year of age`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
